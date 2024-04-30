69871e68baa28d6a668fe5fdff83e163ca355729
Anson-Sun <576585666@qq.com>
2024-04-30 16:15:10 -0400

    页面轮播

---
+++ 页面轮播/test.js
@@ -0,0 +1,39 @@
+/*document.querySelectorAll('.s_button')[1].onclick = () =>{
+    let lists =document.querySelectorAll('.item');
+    document.querySelector('#slide').appendChild(lists[0]);
+}
+
+document.querySelectorAll('.s_button')[0].onclick = () =>{
+    let lists =document.querySelectorAll('.item');
+    document.querySelector('#slide').prepend(lists[lists.length - 1]);
+}*/
+let _direction = true;
+let _move = () =>{
+    let lists =document.querySelectorAll('.item');
+    if(_direction){
+        document.querySelector('#slide').appendChild(lists[0]);
+    }
+    else{
+        document.querySelector('#slide').prepend(lists[lists.length - 1]);
+    }
+}
+
+let timer = setInterval(_move, 2500);
+document.querySelector('.container').addEventListener('mouseover',()=>{
+    clearInterval(timer);
+});
+document.querySelector('.container').addEventListener('mouseout',()=>{
+    timer = setInterval(_move, 2500);
+});
+
+document.querySelectorAll('.s_button')[1].onclick = () =>{
+    clearInterval(timer);
+    _direction = true;
+    _move();
+}
+
+document.querySelectorAll('.s_button')[0].onclick = () =>{
+    clearInterval(timer);
+    _direction = false;
+    _move();
+}
